117348b375fb5e98527089ee1493783a728d58a6
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-06-02 18:16:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone spidyynaik/AutoFilterVerify /DQTheFileDonorBot
+  git clone spidyynaik/AutoFilterVerify /DQTheFileDonor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /DQTheFileDonor
 fi
-cd /DQTheFileDonorBot
+cd /DQTheFileDonor
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
